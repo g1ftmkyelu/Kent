6d01ce9555f31a8226b754bdb379b73c161f14b5
g1ftmkyelu <mkyelugift@gmail.com>
2024-11-04 18:59:58 +0200

    upadte

--- frontend/src/components/portal/render_modes/kanban/kanban_board.vue
+++ frontend/src/components/portal/render_modes/kanban/kanban_board.vue
@@ -487,21 +487,23 @@ const onDrop = async (event, newStatus) => {
     })
 
 
-    const emails=[
-      "gftmkyelu@gmail.com",
-      "transtaxis@yahoo.com",
-      ""
-    ]
+    const emails = [
+  "gftmkyelu@gmail.com",
+  "transtaxis@yahoo.com",
+  "fphiri418@gmail.com",
+  "mkyelugift@gmail.com",
+];
 
+// Iterate through each email and send an email
+for (const email of emails) {
+  await effects.sendEmail({
+    Logo: "https://firebasestorage.googleapis.com/v0/b/server-services-50a49.appspot.com/o/logo%2F%5Bremoval.ai%5D_abd4a3d0-84f8-4c0a-9c3a-8d1f3cedddcd-bbb.png?alt=media&token=0fd681c0-6d3e-4268-9410-f0e38ff2e316",
+    subject: `${props.resource.label.slice(0, -1)} moved to ${newStatus}`,
+    recipientEmail: email, // Send to each email in the array
+    message: message,
+  });
+}
 
-    await effects.sendEmail(
-      {
-         Logo:"https://firebasestorage.googleapis.com/v0/b/server-services-50a49.appspot.com/o/logo%2F%5Bremoval.ai%5D_abd4a3d0-84f8-4c0a-9c3a-8d1f3cedddcd-bbb.png?alt=media&token=0fd681c0-6d3e-4268-9410-f0e38ff2e316",
-         subject: `${props.resource.label.slice(0, -1)} moved to ${newStatus}`,
-         recipientEmail: localStorage.getItem("email"), 
-         message: message 
-      }
-    )
     message.success(`${props.resource.label.slice(0, -1)} moved to ${newStatus}`)
   } catch (error) {
 
